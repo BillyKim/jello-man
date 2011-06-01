44519476655dda1d8c9a2b5c7e60ebb306978f98
seebsprengers@hotmail.com <seebsprengers@hotmail.com@0fb7bab5-1bf9-c5f3-09d9-7611b49293d6>
2011-06-01 17:10:41 +0000

    -Better controls for buttons (light info)

--- trunk/JelloMan/VisualLightDebugger.cpp
+++ trunk/JelloMan/VisualLightDebugger.cpp
@@ -21,20 +21,12 @@ VisualLightDebugger::VisualLightDebugger()	:	m_bClick(false),
 												m_pPowerAddButton(0),
 												m_pPowerSubtractButton(0),
 												m_bTextBoxesSet(false),
-												m_PreviousSelectedLight(9999),
-												m_bLightsMoving(false),
 												m_pLightInfoFont(0),
-												m_pLightInfoFont2(0)
+												m_pLightInfoFont2(0),
+												m_pLight(0),
+												m_pPreviousLight(0),
+												m_bDown(false)
 {
-	
-	// POINT LIGHT
-	m_pPointLightBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/plight_normal.png")));
-	m_pPointLightBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/plight_hover.png")));
-
-	// SPOT LIGHT
-	m_pSpotLightBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/slight_normal.png")));
-	m_pSpotLightBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/slight_hover.png")));
-
 	// ADD - SUBTRACT BUTTON
 	m_pAddSubtractBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/add_small_normal.png")));
 	m_pAddSubtractBitmaps.push_back(Content->LoadImage(_T("../Content/Images/Editor/add_small_hover.png")));
@@ -87,6 +79,8 @@ VisualLightDebugger::VisualLightDebugger()	:	m_bClick(false),
 	// FONT
 	m_pLightInfoFont = Content->LoadTextFormat(_T("Verdana"),10, false,false);
 	m_pLightInfoFont2 = Content->LoadTextFormat(_T("Verdana"),12, false,false);
+
+	m_ClickTimer.Reset();
 }
 
 
@@ -128,144 +122,66 @@ void VisualLightDebugger::Tick(const RenderContext* pRenderContext)
 		m_pTextBoxZ = new TextBox();
 		m_pTextBoxZ->SetBounds(30,145,80,20);
 	}
-}
-
-void VisualLightDebugger::Draw()
-{
-	/*for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)
-	{
-		BX2D->SetAntiAliasing(true);
-		m_pRenderContext->GetLightController()->GetLights()[i]->Draw(m_pRenderContext);
-		BX2D->SetAntiAliasing(false);
-	}*/
-}
-
-void VisualLightDebugger::DeselectAllLights(int excluding)
-{
-	//if (excluding < 0)
-	//{
-	//	for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)
-	//	{
-	//		m_pRenderContext->GetLightController()->GetLights()[i]->Deselect();
-	//	}
-	//}
-	//else
-	//{
-	//	for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)
-	//	{
-	//		if (i != excluding)
-	//			m_pRenderContext->GetLightController()->GetLights()[i]->Deselect();
-	//	}
-	//}
-}
-
-void VisualLightDebugger::ShowLightInfo()
-{
-	tstringstream stream;
-	stream.precision(8);
 
 	if (GetNrLightsSelected() == 1)
 	{
-        LightType type = LightType_Point;
-		int selectedLight = 0;
-
-		for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)
+		if (CONTROLS->LeftMBDown())
 		{
-			if (m_pRenderContext->GetLightController()->GetLights()[i]->IsSelected())
-			{
-				type = m_pRenderContext->GetLightController()->GetLights()[i]->GetType();
-				selectedLight = i;
-			}
+			m_ClickTimer.Tick();
+
+			m_dTime += m_ClickTimer.GetDeltaTime();
 		}
+		else
+		{
+			m_ClickTimer.Reset();
+
+			m_dTime = 0;
+			m_bDown = false;
+		}
+
+		float clickTime = m_ClickTimer.GetGameTime();
+
+		for_each(m_pRenderContext->GetLightController()->GetLights().cbegin(), m_pRenderContext->GetLightController()->GetLights().cend(), [&](Light* pLight)
+		{
+			if (pLight->IsSelected())
+			{
+				m_LightType = pLight->GetType();
+
+				m_pLight = pLight;
+			}
+		});
 
 		m_pMultiplierAddButton->Tick();
 		m_pMultiplierSubtractButton->Tick();
 		m_pAttenuationAddButton->Tick();
 		m_pAttenuationSubtractButton->Tick();
 
-		if (type == LightType_Spot)
+		if (m_LightType == LightType_Spot)
 		{
 			m_pPowerAddButton->Tick();
 			m_pPowerSubtractButton->Tick();
 		}
 
-		Light* pLight = m_pRenderContext->GetLightController()->GetLights()[selectedLight];
-
-		stream << _T("Light ") << selectedLight << _T(":\n\n");
-		tstringstream strm;
-		strm << pLight->GetPosition().X;
-		tstring t = strm.str();
-		stream << _T("X: ")  << _T("\n\n");
-				
-		if (!m_bTextBoxesSet || m_PreviousSelectedLight != selectedLight || m_bLightsMoving)
-		{
-			tstringstream strmX;
-			strmX << t.substr(0, t.find(_T(".")) + 4);
-			m_pTextBoxX->SetText(strmX.str());
-		}
-
-		strm.str(_T(""));
-		strm << pLight->GetPosition().Y;
-		t = strm.str();
-		stream << _T("Y: ") << _T("\n\n");
-
-		if (!m_bTextBoxesSet || m_PreviousSelectedLight != selectedLight || m_bLightsMoving)
-		{
-			tstringstream strmY;
-			strmY << t.substr(0, t.find(_T(".")) + 4);
-			m_pTextBoxY->SetText(strmY.str());
-		}
-
-		strm.str(_T(""));
-		strm << pLight->GetPosition().Z;
-		t = strm.str();
-		stream << _T("Z: ") << _T("\n\n\n");
-
-		if (!m_bTextBoxesSet || m_PreviousSelectedLight != selectedLight || m_bLightsMoving)
-		{
-			tstringstream strmZ;
-			strmZ << t.substr(0, t.find(_T(".")) + 4);
-			m_pTextBoxZ->SetText(strmZ.str());
-		}
-
-		m_bTextBoxesSet = true;
-
-		stream << _T("R:") << static_cast<int>(pLight->GetColor().R * 255) << _T(" ");
-		stream << _T("G:") << static_cast<int>(pLight->GetColor().G * 255) << _T(" ");
-		stream << _T("B:") << static_cast<int>(pLight->GetColor().B * 255) << _T("\n\n\n\n\n\n");
-
-		stream << _T("Multiplier: ") << pLight->GetMulitplier() << _T("\n\n\n\n");
-		stream << _T("Attenuation end: ") << pLight->GetAttenuationEnd() << _T("\n\n\n\n");
-
-        if (type == LightType_Spot)
-		{
-			SpotLight* pSLight = dynamic_cast<SpotLight*>(pLight);
-
-			float angle = ToDegrees(pSLight->GetOpeningsAngle());
-			//if (angle < 0.1f) angle = 0.0f; //would be weird for the debug -- should never be sub 0
-
-			stream << _T("Angle: ") << angle << _T("\n\n\n\n");
-
-			stream << _T("ShadowMaps enabled: ") << pSLight->HasShadowMap() << _T("\n");
-			stream << _T("ShadowMaps size: ") << pSLight->GetShadowMapType();
-		}
-
-		BX2D->SetColor(	static_cast<int>(pLight->GetColor().R * 255),
-							static_cast<int>(pLight->GetColor().G * 255),
-							static_cast<int>(pLight->GetColor().B * 255)	);
-
-		BX2D->FillRect(20, 215, 60, 40);
+		if (m_PreviousPos != m_pLight->GetPosition())
+			m_bTextBoxesSet = false;
 
 		// multiplier
-		float multiplier = pLight->GetMulitplier();
+		float multiplier = m_pLight->GetMulitplier();
 
-		if (m_pMultiplierAddButton->Down())
+		if (!m_bDown)
 		{
-			multiplier += 0.1f;
-		}
-		else if (m_pMultiplierSubtractButton->Down())
-		{
-			multiplier -= 0.1f;
+			if (m_pMultiplierAddButton->Down() && m_dTime > 0.1f || m_pMultiplierAddButton->Clicked())
+			{
+				multiplier += 0.1f;
+
+				m_dTime = 0;
+			}
+			else if (m_pMultiplierSubtractButton->Down() && m_dTime > 0.1f || m_pMultiplierSubtractButton->Clicked())
+			{
+				multiplier -= 0.1f;
+
+				m_dTime = 0;
+			}
 		}
 
 		if (multiplier < 0.1f)
@@ -273,19 +189,26 @@ void VisualLightDebugger::ShowLightInfo()
 			multiplier = 0.0f;
 		}
 
-		pLight->SetMulitplier(multiplier);
+		m_pLight->SetMulitplier(multiplier);
 
 		// attenuation
-		int attenuationEnd = static_cast<int>(pLight->GetAttenuationEnd());
+		int attenuationEnd = static_cast<int>(m_pLight->GetAttenuationEnd());
 
-		if (m_pAttenuationAddButton->Down())
+		if (!m_bDown)
 		{
-			attenuationEnd += 1;
-		}
-			
-		if (m_pAttenuationSubtractButton->Down())
-		{
-			attenuationEnd -= 1;
+			if (m_pAttenuationAddButton->Down() && m_dTime > 0.1f || m_pAttenuationAddButton->Clicked())
+			{
+				attenuationEnd += 1;
+
+				m_dTime = 0;
+			}
+
+			if (m_pAttenuationSubtractButton->Down() && m_dTime > 0.1f || m_pAttenuationSubtractButton->Clicked())
+			{
+				attenuationEnd -= 1;
+
+				m_dTime = 0;
+			}
 		}
 
 		if (attenuationEnd < 1)
@@ -293,32 +216,39 @@ void VisualLightDebugger::ShowLightInfo()
 			attenuationEnd = 0;
 		}
 
-		if (type == LightType_Spot)
+		if (m_LightType == LightType_Spot)
 		{
-			SpotLight* pSLight = dynamic_cast<SpotLight*>(pLight);
+			SpotLight* pSLight = dynamic_cast<SpotLight*>(m_pLight);
 
-			if (m_pPowerAddButton->Down())
+			if (!m_bDown)
 			{
-			    pSLight->AddOpeningsAngle(0.01f); //radians
-		        if (pSLight->GetOpeningsAngle() >= Pi)
-		        {
-			        pSLight->SetOpeningsAngle(Pi - 0.01f);
-		        }
-			}
-			
-			if (m_pPowerSubtractButton->Down())
-			{
-			    pSLight->AddOpeningsAngle(-0.01f);
-		        if (pSLight->GetOpeningsAngle() < 0.01f)
-		        {
-			        pSLight->SetOpeningsAngle(0.01f);
-		        }
+				if (m_pPowerAddButton->Down() && m_dTime > 0.1f || m_pPowerAddButton->Clicked())
+				{
+					pSLight->AddOpeningsAngle(0.01f); //radians
+					if (pSLight->GetOpeningsAngle() >= Pi)
+					{
+						pSLight->SetOpeningsAngle(Pi - 0.01f);
+					}
+
+					m_dTime = 0;
+				}
+
+				if (m_pPowerSubtractButton->Down() && m_dTime > 0.1f || m_pPowerSubtractButton->Clicked())
+				{
+					pSLight->AddOpeningsAngle(-0.01f);
+					if (pSLight->GetOpeningsAngle() < 0.01f)
+					{
+						pSLight->SetOpeningsAngle(0.01f);
+					}
+
+					m_dTime = 0;
+				}
 			}
 		}
 
-		pLight->SetAttenuationEnd(static_cast<float>(attenuationEnd));
+		m_pLight->SetAttenuationEnd(static_cast<float>(attenuationEnd));
 
-		m_PreviousSelectedLight = selectedLight;
+		m_pPreviousLight = m_pLight;
 
 		if (m_pTextBoxX->Entered())
 		{
@@ -334,10 +264,10 @@ void VisualLightDebugger::ShowLightInfo()
 
 			if (!(f == 0 && s2 != "0"))
 			{
-				Vector3 pos = pLight->GetPosition();
+				Vector3 pos = m_pLight->GetPosition();
 				pos.X = f;
 
-				pLight->SetPosition(pos);
+				m_pLight->SetPosition(pos);
 			}
 
 			m_pTextBoxX->LoseFocus();
@@ -357,10 +287,10 @@ void VisualLightDebugger::ShowLightInfo()
 
 			if (!(f == 0 && s2 != "0"))
 			{
-				Vector3 pos = pLight->GetPosition();
+				Vector3 pos = m_pLight->GetPosition();
 				pos.Y = f;
 
-				pLight->SetPosition(pos);
+				m_pLight->SetPosition(pos);
 			}
 
 			m_pTextBoxY->LoseFocus();
@@ -380,41 +310,36 @@ void VisualLightDebugger::ShowLightInfo()
 
 			if (!(f == 0 && s2 != "0"))
 			{
-				Vector3 pos = pLight->GetPosition();
+				Vector3 pos = m_pLight->GetPosition();
 				pos.Z = f;
 
-				pLight->SetPosition(pos);
+				m_pLight->SetPosition(pos);
 			}
 
 			m_pTextBoxZ->LoseFocus();
 		}
-		
-		m_pTextBoxX->Show();
-		m_pTextBoxY->Show();
-		m_pTextBoxZ->Show();
-		
-		m_pMultiplierAddButton->Show();
-		m_pMultiplierSubtractButton->Show();
-		m_pAttenuationAddButton->Show();
-		m_pAttenuationSubtractButton->Show();
 
-		BX2D->SetColor(40,40,40);
-		BX2D->DrawLine(10,180,190,180);
-		BX2D->DrawLine(10,270,190,270);
-		BX2D->DrawLine(10,330,190,330);
-		BX2D->DrawLine(10,387,190,387);
+		m_PreviousPos = m_pLight->GetPosition();
 
-		if (type == LightType_Spot)
-		{
-			m_pPowerAddButton->Show();
-			m_pPowerSubtractButton->Show();
+		if (clickTime < 0.5)
+			m_bDown = true;
+		else
+			m_bDown = false;
+	}
+}
 
-			BX2D->DrawLine(10,445,190,445);
-		}
+void VisualLightDebugger::Draw()
+{
+	if (GetNrLightsSelected() == 1)
+	{
+		//if (m_pLight)
+			ShowLightInfo();
 	}
 	else if (GetNrLightsSelected() > 1)
 	{
-		
+		tstringstream stream;
+		stream.precision(8);
+
 		stream << _T("Selected:\n\n");
 
 		for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)
@@ -425,23 +350,113 @@ void VisualLightDebugger::ShowLightInfo()
 			}
 		}
 
+		BX2D->SetColor(255, 255, 255);
+		BX2D->SetFont(m_pLightInfoFont2);
+		BX2D->DrawString(stream.str(),10,60);
+
 		HideTextBoxes();
 	}
 	else
 		HideTextBoxes();
+}
 
-	/*tstringstream streamL;
+void VisualLightDebugger::ShowLightInfo()
+{
+	tstringstream stream;
+	stream.precision(8);
 
-	streamL << _T("lights: ") << GetNrLightsSelected() << _T(" / ") << GetTotalLightsInScene();
+	if (m_LightType == LightType_Spot)
+		stream << _T("Spotlight selected:\n\n");
+	else
+		stream << _T("Pointlight selected:\n\n");
 
-	BX2D->SetColor(255, 255, 255, 0.5f);
-	BX2D->SetFont(m_pLightInfoFont);
+	tstringstream strm;
+	strm << m_pLight->GetPosition().X;
 
-	m_pLightInfoFont->SetHorizontalAlignment(TEXT_ALIGNMENT_RIGHT);
-	m_pLightInfoFont->SetVerticalAlignment(PARAGRAPH_ALIGNMENT_BOTTOM);
+	tstring t = strm.str();
+	stream << _T("X: ")  << _T("\n\n");
+				
+	if (!m_bTextBoxesSet || m_pPreviousLight != m_pLight)
+	{
+		tstringstream strmX;
+		strmX << t.substr(0, t.find(_T(".")) + 4);
+		m_pTextBoxX->SetText(strmX.str());
+	}
 
-	BX2D->DrawString(	streamL.str(),
-						RectF(0,0, BX2D->GetWindowSize().width - 5, BX2D->GetWindowSize().height - 4));*/
+	strm.str(_T(""));
+	strm << m_pLight->GetPosition().Y;
+	t = strm.str();
+	stream << _T("Y: ") << _T("\n\n");
+
+	if (!m_bTextBoxesSet || m_pPreviousLight != m_pLight)
+	{
+		tstringstream strmY;
+		strmY << t.substr(0, t.find(_T(".")) + 4);
+		m_pTextBoxY->SetText(strmY.str());
+	}
+
+	strm.str(_T(""));
+	strm << m_pLight->GetPosition().Z;
+	t = strm.str();
+	stream << _T("Z: ") << _T("\n\n\n");
+
+	if (!m_bTextBoxesSet || m_pPreviousLight != m_pLight)
+	{
+		tstringstream strmZ;
+		strmZ << t.substr(0, t.find(_T(".")) + 4);
+		m_pTextBoxZ->SetText(strmZ.str());
+	}
+
+	m_bTextBoxesSet = true;
+
+	stream << _T("R:") << static_cast<int>(m_pLight->GetColor().R * 255) << _T(" ");
+	stream << _T("G:") << static_cast<int>(m_pLight->GetColor().G * 255) << _T(" ");
+	stream << _T("B:") << static_cast<int>(m_pLight->GetColor().B * 255) << _T("\n\n\n\n\n\n");
+
+	stream << _T("Multiplier: ") << m_pLight->GetMulitplier() << _T("\n\n\n\n");
+	stream << _T("Attenuation end: ") << m_pLight->GetAttenuationEnd() << _T("\n\n\n\n");
+
+    if (m_LightType == LightType_Spot)
+	{
+		SpotLight* pSLight = dynamic_cast<SpotLight*>(m_pLight);
+
+		float angle = ToDegrees(pSLight->GetOpeningsAngle());
+		//if (angle < 0.1f) angle = 0.0f; //would be weird for the debug -- should never be sub 0
+
+		stream << _T("Angle: ") << angle << _T("\n\n\n\n");
+
+		stream << _T("ShadowMaps enabled: ") << pSLight->HasShadowMap() << _T("\n");
+		stream << _T("ShadowMaps size: ") << pSLight->GetShadowMapType();
+	}
+
+	BX2D->SetColor(	static_cast<int>(m_pLight->GetColor().R * 255),
+					static_cast<int>(m_pLight->GetColor().G * 255),
+					static_cast<int>(m_pLight->GetColor().B * 255)	);
+
+	BX2D->FillRect(20, 215, 60, 40);
+		
+	m_pTextBoxX->Show();
+	m_pTextBoxY->Show();
+	m_pTextBoxZ->Show();
+		
+	m_pMultiplierAddButton->Show();
+	m_pMultiplierSubtractButton->Show();
+	m_pAttenuationAddButton->Show();
+	m_pAttenuationSubtractButton->Show();
+
+	BX2D->SetColor(40,40,40);
+	BX2D->DrawLine(10,180,190,180);
+	BX2D->DrawLine(10,270,190,270);
+	BX2D->DrawLine(10,330,190,330);
+	BX2D->DrawLine(10,387,190,387);
+
+	if (m_LightType == LightType_Spot)
+	{
+		m_pPowerAddButton->Show();
+		m_pPowerSubtractButton->Show();
+
+		BX2D->DrawLine(10,445,190,445);
+	}
 
 	BX2D->SetColor(255, 255, 255);
 	BX2D->SetFont(m_pLightInfoFont2);
@@ -455,12 +470,6 @@ int VisualLightDebugger::GetNrLightsSelected()
 
 	if (m_pRenderContext)
 	{
-		/*for (unsigned int i = 0; i < m_pRenderContext->GetLightController()->GetLights().size(); ++i)	
-		{
-			if (m_pRenderContext->GetLightController()->GetLights()[i]->IsSelected())
-				++b;
-		}*/
-
 		for_each(m_pRenderContext->GetLightController()->GetLights().cbegin(), m_pRenderContext->GetLightController()->GetLights().cend(), [&](Light* pLight)
 		{
 			if (pLight->IsSelected())
--- trunk/JelloMan/VisualLightDebugger.h
+++ trunk/JelloMan/VisualLightDebugger.h
@@ -4,6 +4,7 @@
 #include "Blox2D.h"
 #include "RenderContext.h"
 #include <vector>
+#include "Light.h"
 
 class VisualLightDebugger
 {
@@ -18,30 +19,28 @@ public:
 	void Draw();
 	void ShowLightInfo();
 
-	void DeselectAllLights(int excluding = -1);
-
 	void HideTextBoxes();
-	void MovingLights(bool bMoving)
-	{ m_bLightsMoving = bMoving; }
 
 	// GETTERS
 	int GetNrLightsSelected();
 	int GetTotalLightsInScene();
 
+	// SETTERS
+	void MovingLights(bool bMoving)
+	{ m_bLightsMoving = bMoving; }
+
 private:
 
 	// DATAMEMBERS
-	vector<Image*> m_pPointLightBitmaps;
-	vector<Image*> m_pSpotLightBitmaps;
 	vector<Image*> m_pAddSubtractBitmaps;
 
 	bool m_bClick;
 	bool m_bTextBoxesSet;
 	bool m_bLightsMoving;
+	bool m_bDown;
 
 	int m_TotalSelectedLights;
 	int m_TotalLights;
-	int m_PreviousSelectedLight;
 
 	const RenderContext* m_pRenderContext;
 
@@ -58,4 +57,14 @@ private:
 
 	TextFormat* m_pLightInfoFont;
 	TextFormat* m_pLightInfoFont2;
+
+	LightType m_LightType;
+	Light* m_pLight;
+	Light* m_pPreviousLight;
+
+	Vector3 m_PreviousPos;
+
+	GameTimer m_ClickTimer;
+
+	float m_dTime;
 };
